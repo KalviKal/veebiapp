7c18ac878c6d173e14f2832b20b95f94af5a07a3
KalviKal <66356112+KalviKal@users.noreply.github.com>
2025-02-20 11:09:09 +0200

    harjutus1 second endpoint

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const express = require('express')
 
 const app = express()
 
+
+
 app.get('/tervitus', (req, res) => {
+    console.log(req.query)
+    res.send("Tere maailm!")
+})
+
+app.get('/harjutus1', (req, res) => {
     console.log(req.query)
     res.send(`
         <html>
+
+           
             <head>
+                <style>
+                    h1 {
+                    color: red;
+                    }
+                </style>
             </head>
             <body>
-                Tervitused ${req.query.nimi}
+                <h1>${req.query.nimi} on maailma parim!</h1>
             </body>
         </html>
         `)
