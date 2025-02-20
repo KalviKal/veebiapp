9f54ded96892feb6750c4890e99bd3453f3a4e9d
KalviKal <66356112+KalviKal@users.noreply.github.com>
2025-02-20 09:42:23 +0200

    homeowork1

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const express = require('express')
 const app = express()
 
 app.get('/tervitus', (req, res) => {
-    res.end('Tere maailm')
+    console.log(req.query)
+    res.send(`
+        <html>
+            <head>
+            </head>
+            <body>
+                Tervitused ${req.query.nimi}
+            </body>
+        </html>
+        `)
 })
 
 app.listen(3200)
